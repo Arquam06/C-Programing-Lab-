d678f7dcf57eb24f4fba36c908ac8aade6b4ff28
M Arquam Kamal <marquamkamal7@gmail.com>
2025-10-11 16:01:40 +0530

    Update and rename Programing 31.cpp to Programing-31.c

--- Day 6/Programing-31.c
+++ Day 6/Programing-31.c
@@ -10,7 +10,7 @@ int main()
     printf("\nMultiplication Table of %d:\n", n);
     for (i = 1; i <= 10; i++)
     {
-        printf("%d × %d = %d\n", n, i, n * i);
+        printf("%d Ã— %d = %d\n", n, i, n * i);
     }
 
     return 0;
