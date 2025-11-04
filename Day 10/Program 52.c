69082c39f5e2226edf0433c4673bf0e246413b9c
M Arquam Kamal <marquamkamal7@gmail.com>
2025-11-04 23:03:46 +0530

    Refactor main function for better formatting

--- Day 10/Program 52.c
+++ Day 10/Program 52.c
@@ -1,16 +1,22 @@
-  
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+    int i, j, k;
     char ch = 'A';
-    for (int i = 1; i <= 4; i++) {
-        for (int j = i; j < 4; j++)
-            printf(" ");
-        for (int k = 1; k <= i; k++) {
+    for (i = 1; i <= 4; i++) 
+    {
+        for (k = 4; k > i; k--) 
+        {
+            printf("  ");
+        }
+        for (j = 1; j <= i; j++) 
+        {
             printf("%c ", ch);
             ch++;
         }
         printf("\n");
     }
+
     return 0;
 }
